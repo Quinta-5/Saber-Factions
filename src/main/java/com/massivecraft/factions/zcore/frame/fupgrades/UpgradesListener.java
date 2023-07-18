b3301bf0bb631353c42dbb025085430d1fe1c931
Quinta-5 <139925132+Quinta-5@users.noreply.github.com>
2023-07-18 18:13:07 -0400

    Update to Current

--- src/main/java/com/massivecraft/factions/zcore/frame/fupgrades/UpgradesListener.java
+++ src/main/java/com/massivecraft/factions/zcore/frame/fupgrades/UpgradesListener.java
@@ -1,17 +1,21 @@
 package com.massivecraft.factions.zcore.frame.fupgrades;
 
 import com.cryptomorin.xseries.XMaterial;
-import com.massivecraft.factions.*;
+import com.massivecraft.factions.Board;
+import com.massivecraft.factions.FLocation;
+import com.massivecraft.factions.FPlayer;
+import com.massivecraft.factions.FPlayers;
+import com.massivecraft.factions.Faction;
+import com.massivecraft.factions.FactionsPlugin;
 import com.massivecraft.factions.util.FastMath;
 import com.massivecraft.factions.util.Logger;
 import com.massivecraft.factions.zcore.frame.fupgrades.provider.stackers.RoseStackerProvider;
 import com.massivecraft.factions.zcore.frame.fupgrades.provider.stackers.WildStackerProvider;
 import org.bukkit.Bukkit;
-import org.bukkit.CropState;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockState;
 import org.bukkit.block.CreatureSpawner;
+import org.bukkit.block.data.Ageable;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -22,11 +26,9 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.SpawnerSpawnEvent;
 import org.bukkit.event.player.PlayerItemDamageEvent;
-import org.bukkit.material.Crops;
 import org.bukkit.plugin.Plugin;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class UpgradesListener implements Listener {
@@ -61,7 +63,7 @@ public class UpgradesListener implements Listener {
         FLocation floc = FLocation.wrap(e.getEntity().getLocation());
         Faction faction = Board.getInstance().getFactionAt(floc);
         if (!faction.isWilderness()) {
-            int level = faction.getUpgrade("EXP");
+            int level = faction.getUpgrade(UpgradeType.EXP);
             double multiplier = FactionsPlugin.getInstance().getFileManager().getUpgrades().getConfig().getDouble("fupgrades.MainMenu.EXP.EXP-Boost.level-" + level);
             if (level != 0 && multiplier > 0.0) {
                 this.spawnMoreExp(e, multiplier);
@@ -79,7 +81,7 @@ public class UpgradesListener implements Listener {
         FLocation floc = FLocation.wrap(e.getLocation());
         Faction factionAtLoc = Board.getInstance().getFactionAt(floc);
         if (!factionAtLoc.isWilderness()) {
-            int level = factionAtLoc.getUpgrade("Spawners");
+            int level = factionAtLoc.getUpgrade(UpgradeType.SPAWNER);
             if (level == 0) return;
             this.lowerSpawnerDelay(e, FactionsPlugin.getInstance().getFileManager().getUpgrades().getConfig().getDouble("fupgrades.MainMenu.Spawners.Spawner-Boost.level-" + level));
         }
@@ -101,7 +103,7 @@ public class UpgradesListener implements Listener {
         FLocation floc = FLocation.wrap(e.getBlock().getLocation());
         Faction factionAtLoc = Board.getInstance().getFactionAt(floc);
         if (!factionAtLoc.isWilderness()) {
-            int level = factionAtLoc.getUpgrade("Crops");
+            int level = factionAtLoc.getUpgrade(UpgradeType.CROP);
             int chance = FactionsPlugin.getInstance().getFileManager().getUpgrades().getConfig().getInt("fupgrades.MainMenu.Crops.Crop-Boost.level-" + level);
             if (level == 0 || chance == 0) return;
 
@@ -111,22 +113,38 @@ public class UpgradesListener implements Listener {
     }
 
     private void growCrop(BlockGrowEvent e) {
-        if (e.getBlock().getType().equals(XMaterial.WHEAT.parseMaterial())) {
-            e.setCancelled(true);
-            Crops c = new Crops(CropState.RIPE);
-            BlockState bs = e.getBlock().getState();
-            bs.setData(c);
-            bs.update();
+        Material type = e.getBlock().getType();
+        if (cropMaterials == null) {
+            cropMaterials = EnumSet.of(
+                    XMaterial.WHEAT.parseMaterial(),
+                    XMaterial.BEETROOT.parseMaterial(),
+                    XMaterial.CARROTS.parseMaterial(),
+                    XMaterial.POTATOES.parseMaterial(),
+                    XMaterial.NETHER_WART.parseMaterial(),
+                    XMaterial.COCOA.parseMaterial()
+            );
+            sugarCaneMaterial = XMaterial.SUGAR_CANE.parseMaterial();
         }
-        Block below = e.getBlock().getLocation().subtract(0.0D, 1.0D, 0.0D).getBlock();
-        if (below.getType() == XMaterial.SUGAR_CANE.parseMaterial()) {
-            Block above = e.getBlock().getLocation().add(0.0D, 1.0D, 0.0D).getBlock();
-            if (above.getType() == Material.AIR && above.getLocation().add(0.0D, -2.0D, 0.0D).getBlock().getType() != Material.AIR)
-                above.setType(XMaterial.SUGAR_CANE.parseMaterial());
-        } else if (below.getType() == Material.CACTUS) {
-            Block above = e.getBlock().getLocation().add(0.0D, 1.0D, 0.0D).getBlock();
-            if (above.getType() == Material.AIR && above.getLocation().add(0.0D, -2.0D, 0.0D).getBlock().getType() != Material.AIR)
-                above.setType(Material.CACTUS);
+        if (cropMaterials.contains(type)) { // ageable single block crop (wheat, beetroot, etc.)
+            e.setCancelled(true);
+            Ageable ageable = (Ageable) e.getBlock().getBlockData();
+            int newAge = Math.min(ageable.getAge() + 2, ageable.getMaximumAge());
+            ageable.setAge(newAge);
+            e.getBlock().setBlockData(ageable);
+        } else { // multi-block crop (cactus, sugarcane)
+            Block above = e.getBlock().getLocation().add(0.0, 1.0, 0.0).getBlock();
+            Block below = e.getBlock().getLocation().add(0.0, -1.0, 0.0).getBlock();
+            Material aboveType = above.getType();
+            Material belowType = below.getType();
+            if (belowType == sugarCaneMaterial) {
+                if (aboveType == Material.AIR && above.getLocation().add(0.0, -2.0, 0.0).getBlock().getType() != Material.AIR) {
+                    above.setType(sugarCaneMaterial);
+                }
+            } else if (belowType == Material.CACTUS) {
+                if (aboveType == Material.AIR && above.getLocation().add(0.0, -2.0, 0.0).getBlock().getType() != Material.AIR) {
+                    above.setType(Material.CACTUS);
+                }
+            }
         }
     }
 
@@ -136,7 +154,7 @@ public class UpgradesListener implements Listener {
         Faction factionAtLoc = Board.getInstance().getFactionAt(floc);
 
         if (!factionAtLoc.isWilderness()) {
-            int level = factionAtLoc.getUpgrade("Redstone");
+            int level = factionAtLoc.getUpgrade(UpgradeType.REDSTONE);
             if (level != 0) {
                 if (level == 1) {
                     List<String> unbreakable = FactionsPlugin.getInstance().getConfig().getStringList("no-water-destroy.Item-List");
@@ -157,7 +175,7 @@ public class UpgradesListener implements Listener {
                 FPlayer fPlayer = FPlayers.getInstance().getByPlayer(player);
 
                 if (fPlayer.getFaction().isNormal()) {
-                    int level = fPlayer.getFaction().getUpgrade("Fall-Damage");
+                    int level = fPlayer.getFaction().getUpgrade(UpgradeType.FALL_DAMAGE);
 
                     FLocation fLocation = FLocation.wrap(player.getLocation());
                     if (Board.getInstance().getFactionAt(fLocation) == fPlayer.getFaction() && level > 0) {
@@ -173,7 +191,7 @@ public class UpgradesListener implements Listener {
         if (FPlayers.getInstance().getByPlayer(e.getPlayer()) == null) return;
 
         if (e.getItem().getType().toString().contains("LEGGINGS") || e.getItem().getType().toString().contains("CHESTPLATE") || e.getItem().getType().toString().contains("HELMET") || e.getItem().getType().toString().contains("BOOTS")) {
-            int lvl = FPlayers.getInstance().getByPlayer(e.getPlayer()).getFaction().getUpgrade("Armor");
+            int lvl = FPlayers.getInstance().getByPlayer(e.getPlayer()).getFaction().getUpgrade(UpgradeType.REINFORCEDARMOR);
             double drop = FactionsPlugin.getInstance().getFileManager().getUpgrades().getConfig().getDouble("fupgrades.MainMenu.Armor.Armor-HP-Drop.level-" + lvl);
             int newDamage = FastMath.round(e.getDamage() - e.getDamage() * drop);
             e.setDamage(newDamage);
